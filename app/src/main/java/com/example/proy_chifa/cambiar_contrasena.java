71739d650e5d8b65dc4953faa8f2e356a56b756a
unknown <caparachindavid@gmail.com>
2019-04-15 15:51:04 -0500

    Iniciando el proyecto

---
+++ app/src/main/java/com/example/proy_chifa/cambiar_contrasena.java
@@ -0,0 +1,13 @@
+package com.example.proy_chifa;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class cambiar_contrasena extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cambiar_contrasena);
+    }
+}
