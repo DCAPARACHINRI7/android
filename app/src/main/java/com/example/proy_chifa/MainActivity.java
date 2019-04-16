6327cd7e0e2b1d6efa403d727b8e0ed7f2947434
leonardo huamani <leohuamani471@gmail.com>
2019-04-15 19:56:33 -0500

    boton

--- app/src/main/java/com/example/proy_chifa/MainActivity.java
+++ app/src/main/java/com/example/proy_chifa/MainActivity.java
@@ -1,13 +1,28 @@
 package com.example.proy_chifa;
 
+import android.content.Intent;
+import android.sax.StartElementListener;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button bingresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bingresar = (Button) findViewById(R.id.id_login);
+
+        bingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, login.class);
+                startActivity(intent);
+            }
+        });
     }
 }
