86b744696a8d6c7f0fb50f0c5d027f69f4b5ea41
JairoAtoche <jairoAtoche@gmail.com>
2019-04-16 10:55:14 -0500

    botones

--- app/src/main/java/com/example/proy_chifa/login.java
+++ app/src/main/java/com/example/proy_chifa/login.java
@@ -1,13 +1,46 @@
 package com.example.proy_chifa;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
+
+    Button bingresar, bregistrar;
+    TextView idreestalecer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        bingresar = (Button) findViewById(R.id.bingresar);
+        bregistrar = (Button) findViewById(R.id.bregistrar);
+        idreestalecer = (TextView) findViewById(R.id.idreestablecer);
+
+        idreestalecer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, restablecer_contrasena.class);
+                startActivity(intent);
+            }
+        });
+        bingresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        bregistrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login.this, registro.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
